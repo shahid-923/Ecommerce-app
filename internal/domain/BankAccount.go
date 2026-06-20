e1374c61f3c70072221b924a572f860b1ea58707
Md Shahid Ansari <sa160030@gmail.com>
2026-06-20 08:48:22 +0530

    Added BankAccount struct

---
+++ internal/domain/BankAccount.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type BankAccount struct {
+    ID          uint      `json:"id" gorm:"primaryKey"`
+    UserID      uint      `json:"user_id"`
+    BankAccountNumber string  `json:"account_number" gorm:"unique;not null"` 
+    SwiftCode   string    `json:"swift_code"`
+    PaymentType string    `json:"payment_type"`
+    CreatedAt   time.Time `json:"created_at" gorm:"default:current_timestamp"`
+    UpdatedAt   time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+}
