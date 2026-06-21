d9c7d92811fb277a9a01c9d3f7a98ed731921a0a
Md Shahid Ansari <sa160030@gmail.com>
2026-06-21 09:21:24 +0530

    chnaged bankAccount no to uint

--- internal/domain/BankAccount.go
+++ internal/domain/BankAccount.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type BankAccount struct {
-    ID          uint      `json:"id" gorm:"primaryKey"`
-    UserID      uint      `json:"user_id"`
-    BankAccountNumber string  `json:"account_number" gorm:"unique;not null"` 
-    SwiftCode   string    `json:"swift_code"`
-    PaymentType string    `json:"payment_type"`
-    CreatedAt   time.Time `json:"created_at" gorm:"default:current_timestamp"`
-    UpdatedAt   time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+    ID                uint      `json:"id" gorm:"primaryKey"`
+    UserID            uint      `json:"user_id"`
+    BankAccountNumber uint      `json:"account_number" gorm:"unique;not null"` // 👈 Changed string to uint
+    SwiftCode         string    `json:"swift_code"`
+    PaymentType       string    `json:"payment_type"`
+    CreatedAt         time.Time `json:"created_at" gorm:"default:current_timestamp"`
+    UpdatedAt         time.Time `json:"updated_at" gorm:"default:current_timestamp"`
 }
